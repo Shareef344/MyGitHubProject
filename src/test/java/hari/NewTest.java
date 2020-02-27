f5ee2df0d0bbaa44254204cc1e57372046b199f6
Shareef344 <61041128+Shareef344@users.noreply.github.com>
2020-02-27 11:34:00 +0530

    Add files via upload

---
+++ src/test/java/hari/NewTest.java
@@ -0,0 +1,34 @@
+package hari;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+	WebDriver driver;
+  @Test
+  public void f() {
+	  driver.findElement(By.name("username")).sendKeys("admin");
+		driver.findElement(By.name("password")).sendKeys("admin");
+		driver.findElement(By.name("submit")).click();
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  System.setProperty("webdriver.chrome.driver", "C:\\lib\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("http://selenium4testing.com/hms/");
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
